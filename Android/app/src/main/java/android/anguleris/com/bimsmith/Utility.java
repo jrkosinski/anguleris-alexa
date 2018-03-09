32c0737b3ab2286a7ab7043ebb884412bbb54f29
John R. Kosinski <john.kosinski@gmail.com>
2018-03-09 18:09:27 +0700

    Repo rearrangement for Android

---
+++ Android/app/src/main/java/android/anguleris/com/bimsmith/Utility.java
@@ -0,0 +1,25 @@
+package android.anguleris.com.bimsmith;
+
+import android.content.ActivityNotFoundException;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+/**
+ * Created by Home on 9/3/2561.
+ */
+public class Utility {
+    public static void callPhone(ContextWrapper context, String number) {
+        try {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + number));
+            context.startActivity(callIntent);
+
+        } catch (ActivityNotFoundException e) {
+            Log.e("Calling a Phone Number", "Call failed", e);
+        } catch(SecurityException e) {
+            Log.e("No permission", "No permission", e);
+        }
+    }
+}
